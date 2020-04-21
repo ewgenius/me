d00ecf22627ac29982def1efde8fa4ef760f0eec
Evgenii Khramkov <ewgeniux@gmail.com>
2020-04-21 11:30:05 -0700

    add theming support

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -15,65 +15,6 @@ export default function Home() {
         <h1 className="title">ewgenius</h1>
         <p className="description">Making things</p>
       </main>
-
-      <style jsx>{`
-        .container {
-          min-height: 100vh;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-
-        main {
-          flex: 1;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-
-        .avatar {
-          height: 128px;
-          width: 128px;
-        }
-
-        a {
-          color: inherit;
-          text-decoration: none;
-        }
-
-        .title {
-          margin: 0;
-          line-height: 1.15;
-          font-size: 2rem;
-        }
-
-        .title,
-        .description {
-          text-align: center;
-        }
-
-        .description {
-          line-height: 1.5;
-          font-size: 1rem;
-        }
-      `}</style>
-
-      <style jsx global>{`
-        html,
-        body {
-          padding: 0;
-          margin: 0;
-          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
-            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
-            sans-serif;
-        }
-
-        * {
-          box-sizing: border-box;
-        }
-      `}</style>
     </div>
   );
 }
