b2148e30f4aaa4ac3a7ecf813da9f0e79a796263
Evgenii Khramkov <ewgeniux@gmail.com>
2020-05-24 22:44:29 -0700

    add footer and links

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,9 @@
 import Head from "next/head";
+import { RiInstagramLine, RiTwitterLine, RiGithubLine } from "react-icons/ri";
 
 export default function Home() {
   return (
-    <div className="container">
+    <>
       <Head>
         <title>ewgenius</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
@@ -10,11 +11,28 @@ export default function Home() {
         <link rel="manifest" href="/site.webmanifest" />
       </Head>
 
-      <main>
-        <img className="avatar" src="/images/avatar.png" alt="logo" />
-        <h1 className="title">ewgenius</h1>
-        <p className="description">Making things...</p>
-      </main>
-    </div>
+      <div className="container">
+        <main>
+          <img className="avatar" src="/images/avatar.png" alt="logo" />
+          <h1 className="title">ewgenius</h1>
+          <p className="description">Making things...</p>
+        </main>
+
+        <footer>
+          <div className="links">
+            <a href="https://instagram.com/ewgeniux" target="__blank">
+              <RiInstagramLine />
+            </a>
+            <a href="https://twitter.com/ewgeniux" target="__blank">
+              <RiTwitterLine />
+            </a>
+            <a href="https://github.com/ewgenius" target="__blank">
+              <RiGithubLine />
+            </a>
+          </div>
+          <small>&copy; 2020, ewgenius.me</small>
+        </footer>
+      </div>
+    </>
   );
 }
