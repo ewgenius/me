3debbcbcee9c00433108eca9bc0b1a1cbe84f529
Evgenii Khramkov <ewgeniux@gmail.com>
2020-05-28 15:45:22 -0700

    use proper size

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -7,13 +7,13 @@ export const Layout = ({ children }: PropsWithChildren<{}>) => (
     <footer>
       <div className="links">
         <a href="https://github.com/ewgenius" target="__blank">
-          <GitHub />
+          <GitHub size={16} />
         </a>
         <a href="https://instagram.com/ewgeniux" target="__blank">
-          <Instagram />
+          <Instagram size={16} />
         </a>
         <a href="https://twitter.com/ewgeniux" target="__blank">
-          <Twitter />
+          <Twitter size={16} />
         </a>
       </div>
       <small>&copy; 2020, ewgenius.me</small>
