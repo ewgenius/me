c5d845ee67743ca1c8945641b1990539662d0575
Evgenii Khramkov <ewgeniux@gmail.com>
2020-06-16 17:07:24 -0700

    more details for resume

--- src/pages/resume.tsx
+++ src/pages/resume.tsx
@@ -9,6 +9,7 @@ export interface Job {
   id: number;
   title: string;
   company: string;
+  description: string[];
   startDate: string;
   endDate?: string;
   tags?: string[];
@@ -18,13 +19,39 @@ export interface ResumeProps {
   jobs: Dictionary<Job>;
 }
 
+export const Job = ({ job }: { job: Job }) => {
+  return (
+    <div className="job" key={job.id}>
+      <b>{job.title}</b>
+      <p className="job-details job-company">{job.company}</p>
+      <p className="job-details job-date">
+        {job.startDate} - {job.endDate || "now"}
+      </p>
+      {job.description && (
+        <div className="job-details job-description">
+          {job.description.map((line) => (
+            <p>{line}</p>
+          ))}
+        </div>
+      )}
+      <div className="tags">
+        {job.tags && job.tags.map((tag) => <div className="tag">{tag}</div>)}
+      </div>
+    </div>
+  );
+};
+
 export default function Resume(props: ResumeProps) {
   return (
     <Layout>
       <h1>Resume</h1>
       <section className="resume-section">
-        <h3>About</h3>
+        <h3>About me</h3>
         <p>My name is Evgeniy Khramkov</p>
+        <p>
+          I'm software developer with 7+ years experience in backend and
+          frontend development
+        </p>
       </section>
 
       <section className="resume-section">
@@ -35,13 +62,7 @@ export default function Resume(props: ResumeProps) {
         {Object.values(props.jobs)
           .sort((j1, j2) => j2.id - j1.id)
           .map((job) => (
-            <div className="job" key={job.id}>
-              <b>{job.title}</b>
-              <p className="job-details job-date">
-                {job.startDate} - {job.endDate || "now"}
-              </p>
-              <p className="job-details job-company">{job.company}</p>
-            </div>
+            <Job key={job.id} job={job} />
           ))}
       </section>
 
@@ -67,6 +88,7 @@ export const getStaticProps: GetStaticProps<ResumeProps> = async () => {
           id: 1,
           title: "junior web-developer",
           company: "Brandmaker",
+          description: [""],
           startDate: formatJobDate(new Date("Apr 01, 2011")),
           endDate: formatJobDate(new Date("Aug 31, 2011")),
           tags: ["css", "html", "js"],
@@ -75,6 +97,7 @@ export const getStaticProps: GetStaticProps<ResumeProps> = async () => {
           id: 2,
           title: "junior web-developer",
           company: "Media Plus",
+          description: [""],
           startDate: formatJobDate(new Date("Jul 01, 2013")),
           endDate: formatJobDate(new Date("Aug 31, 2013")),
           tags: ["css", "js", "webgl"],
@@ -83,6 +106,7 @@ export const getStaticProps: GetStaticProps<ResumeProps> = async () => {
           id: 3,
           title: "web-developer",
           company: "Yellow Pages Telecom",
+          description: [""],
           startDate: formatJobDate(new Date("Oct 01, 2013")),
           endDate: formatJobDate(new Date("Apr 01, 2014")),
           tags: ["django", "js", "postgresql", "python"],
@@ -91,6 +115,7 @@ export const getStaticProps: GetStaticProps<ResumeProps> = async () => {
           id: 4,
           title: "web-developer",
           company: "DigitalBox",
+          description: [""],
           startDate: formatJobDate(new Date("May 01, 2014")),
           endDate: formatJobDate(new Date("Apr 01, 2017")),
           tags: ["angular.js", "node.js", "react.js", "typescript"],
@@ -99,6 +124,7 @@ export const getStaticProps: GetStaticProps<ResumeProps> = async () => {
           id: 5,
           title: "software engineer",
           company: "Akvelon",
+          description: [""],
           startDate: formatJobDate(new Date("Apr 10, 2017")),
           endDate: formatJobDate(new Date("Jan 14, 2018")),
           tags: ["CI/CD", "node.js", "react.js", "typescript"],
@@ -107,6 +133,7 @@ export const getStaticProps: GetStaticProps<ResumeProps> = async () => {
           id: 6,
           title: "software engineer 2",
           company: "Microsoft (Rus)",
+          description: [""],
           startDate: formatJobDate(new Date("Jan 15, 2018")),
           endDate: formatJobDate(new Date("May 19, 2019")),
           tags: ["CI/CD", "node.js", "react.js", "typescript"],
@@ -115,6 +142,10 @@ export const getStaticProps: GetStaticProps<ResumeProps> = async () => {
           id: 7,
           title: "software engineer 2",
           company: "Microsoft (US)",
+          description: [
+            "full-stack development of App Center (Build and Diagnostics services)",
+            "Frontend - worked on UI for Build and Diagnostics Search services",
+          ],
           startDate: formatJobDate(new Date("May 20, 2019")),
           endDate: null,
           tags: ["CI/CD", "node.js", "react.js", "typescript"],
