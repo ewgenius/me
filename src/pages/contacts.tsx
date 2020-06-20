3b03cd51802c3ac9f844e74c89cb49642cddf426
Evgenii Khramkov <ewgeniux@gmail.com>
2020-06-20 15:25:12 -0700

    add contacts page

--- src/pages/contacts.tsx
+++ src/pages/contacts.tsx
@@ -1,5 +1,35 @@
 import { Layout } from "../components/Layout";
+import { Mail, Linkedin, MessageCircle } from "react-feather";
 
 export default function Contacts() {
-  return <Layout title="Contacts"></Layout>;
+  return (
+    <Layout title="Contacts">
+      <ul className="contacts">
+        <li>
+          <a href="mailto:ewgeniux@gmail.com">
+            <Mail size={16} />
+            ewgeniux@gmail.com
+          </a>
+        </li>
+        <li>
+          <a href="https://www.linkedin.com/in/ewgenius/" target="__blank">
+            <Linkedin size={16} />
+            linkedin/ewgenius
+          </a>
+        </li>
+        <li>
+          <a href="https://t.me/ewgenius" target="__blank">
+            <MessageCircle size={16} />
+            t.me/ewgenius
+          </a>
+        </li>
+        <li>
+          <a href="https://vk.com/iewgenius" target="__blank">
+            <MessageCircle size={16} />
+            vk.com/iewgenius
+          </a>
+        </li>
+      </ul>
+    </Layout>
+  );
 }
