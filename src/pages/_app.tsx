7975a37bd316bdc8d64d3ce41055b05b1b540ebb
Evgenii Khramkov <ewgeniux@gmail.com>
2020-06-20 16:29:33 -0700

    add description

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -8,6 +8,7 @@ export default function App({ Component, pageProps }: AppPropsType) {
       <Head>
         <title>ewgenius</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="description" content="Evgenii Khramkov personal page" />
         <link rel="icon" href="/favicon.ico" />
         <link rel="manifest" href="/site.webmanifest" />
       </Head>
