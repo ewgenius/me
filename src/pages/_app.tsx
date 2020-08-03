420dd5ac0893f39620199280a32916b8f49535d4
Evgenii Khramkov <ewgeniux@gmail.com>
2020-08-03 07:16:52 -0700

    add fahtom analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,31 @@
 import Head from "next/head";
 import { AppPropsType } from "next/dist/next-server/lib/utils";
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+import * as Fathom from "fathom-client";
 import "../styles/index.css";
 
 export default function App({ Component, pageProps }: AppPropsType) {
+  const router = useRouter();
+
+  useEffect(() => {
+    // Initialize Fathom when the app loads
+    Fathom.load("TDBSHEND", {
+      includedDomains: ["ewgenius.me", "localhost"],
+    });
+
+    function onRouteChangeComplete() {
+      Fathom.trackPageview();
+    }
+    // Record a pageview when route changes
+    router.events.on("routeChangeComplete", onRouteChangeComplete);
+
+    // Unassign event listener
+    return () => {
+      router.events.off("routeChangeComplete", onRouteChangeComplete);
+    };
+  }, []);
+
   return (
     <>
       <Head>
