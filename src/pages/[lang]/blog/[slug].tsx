8078bb0dc5d62d44a2a25165df60fb2c0fb38411
Evgenii Khramkov <ewgeniux@gmail.com>
2020-08-07 23:47:40 -0700

    pass description

--- src/pages/[lang]/blog/[slug].tsx
+++ src/pages/[lang]/blog/[slug].tsx
@@ -9,7 +9,7 @@ export type PostViewProps = PropsWithLocale<{
 
 export default function PostView({ post }: PostViewProps) {
   return (
-    <Layout title={post.title}>
+    <Layout title={post.title} description={post.meta_description}>
       {post.feature_image && (
         <img className="rounded-lg" src={post.feature_image} />
       )}
