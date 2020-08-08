31475064b8be02a25c9883e6d29a40f314554567
Evgenii Khramkov <ewgeniux@gmail.com>
2020-08-07 19:26:08 -0700

    update styles

--- src/components/JobCard.tsx
+++ src/components/JobCard.tsx
@@ -6,7 +6,14 @@ export const JobCard = ({ job }: { job: Job }) => {
   const description =
     (lang === "ru" && job.description_ru) || job.description_en;
   return (
-    <div className="mb-4 pb-1 pl-4 border-l-2 border-gray-500" key={job.id}>
+    <div
+      className="mb-4 relative ml-3 pb-1 pl-5 border-l-2 border-gray-500"
+      key={job.id}
+    >
+      <div
+        className="absolute border-2 border-gray-500 rounded-full w-4 h-4 bg-white"
+        style={{ left: "-9px", top: "8px" }}
+      />
       <b className="text-xl">{job.name}</b>
       <p className="text-sm text-gray-700 font-bold">{job.company}</p>
       <p className="text-sm text-gray-700">
