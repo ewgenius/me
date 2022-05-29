10cdb72e4284b28979bc89206d7722eeaf4cbda9
Evgenii Khramkov <ewgeniux@gmail.com>
2022-05-29 22:44:36 +0300

    track only prod

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -8,6 +8,7 @@ export default function Document() {
           async
           defer
           data-website-id="89bea2e4-72e4-4c33-a0e3-576f233cc33b"
+          data-domains="ewgenius.me"
           src="https://analytics.ewgenius.me/umami.js"
         ></script>
       </Head>
