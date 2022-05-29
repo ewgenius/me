1abfa343237ab40a6b79abf74ba9d16dcdf0b74a
Evgenii Khramkov <ewgeniux@gmail.com>
2022-05-29 22:42:35 +0300

    switch from fathom to umami

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,18 +2,13 @@ import "tailwindcss/tailwind.css";
 import type { AppProps } from "next/app";
 import { useEffect } from "react";
 import { useRouter } from "next/router";
-import * as Fathom from "fathom-client";
 
 function MyApp({ Component, pageProps }: AppProps) {
   const router = useRouter();
 
   useEffect(() => {
-    Fathom.load("IYQFNUUM", {
-      includedDomains: ["ewgenius.me"],
-    });
-
     function onRouteChangeComplete() {
-      Fathom.trackPageview();
+
     }
 
     router.events.on("routeChangeComplete", onRouteChangeComplete);
---
+++ src/pages/_document.tsx
@@ -0,0 +1,20 @@
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html>
+      <Head>
+        <script
+          async
+          defer
+          data-website-id="89bea2e4-72e4-4c33-a0e3-576f233cc33b"
+          src="https://analytics.ewgenius.me/umami.js"
+        ></script>
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
