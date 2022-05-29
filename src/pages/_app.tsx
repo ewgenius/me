6fbc99fa1e946ecd3d428e6a25537e677a7211e5
Evgenii Khramkov <ewgeniux@gmail.com>
2022-05-29 22:45:33 +0300

    cleanup

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,23 +1,7 @@
 import "tailwindcss/tailwind.css";
 import type { AppProps } from "next/app";
-import { useEffect } from "react";
-import { useRouter } from "next/router";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  const router = useRouter();
-
-  useEffect(() => {
-    function onRouteChangeComplete() {
-
-    }
-
-    router.events.on("routeChangeComplete", onRouteChangeComplete);
-
-    return () => {
-      router.events.off("routeChangeComplete", onRouteChangeComplete);
-    };
-  }, []);
-
   return <Component {...pageProps} />;
 }
 export default MyApp;
